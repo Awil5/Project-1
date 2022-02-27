09278b411bc98cd9fe3c392ef513866c3b0f98c3
Awil5 <99454667+Awil5@users.noreply.github.com>
2022-02-27 11:14:56 -0600

    Add files via upload

---
+++ Linux/crnts.sh
@@ -0,0 +1,9 @@
+mkdir Player_Analysis
+Mkdir Dealer_Analysis
+cd Dealer_Analysis
+touch Notes_Dealer_Analysis
+cd ..
+mkdir Player_Dealer_Correlation
+cd Player_Dealer_Correlation
+touch Notes_Player_Dealer_Correlation
+
